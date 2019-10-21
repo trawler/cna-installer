41f052ebb7edfc402ad74e24d141c25b3ff551fc
Karen Almog <wrd4wrd@gmail.com>
2019-10-21 17:28:03 +0200

    make lint happy

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2019 Karen Almog
-
-*/
 package cmd
 
 import (
--- main.go
+++ main.go
@@ -1,11 +1,7 @@
-/*
-Copyright © 2019 Karen Almog
-
-*/
 package main
 
 import "github.com/trawler/cna-installer/cmd"
 
 func main() {
-  cmd.Execute()
+	cmd.Execute()
 }
